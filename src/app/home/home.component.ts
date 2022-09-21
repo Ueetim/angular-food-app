2f8b455d100eb0eebb19662c8458edf603d0cb2c
uduak <lorduduak@gmail.com>
2022-09-21 09:56:09 +0100

    inject food service ce and display foods in the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // declare food property
+  foods:String[] = [];
+
+  constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
+    // fill the food variable with foods from the food service
+    this.foods = this.foodService.getAll();
   }
 
 }
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -10,7 +10,12 @@ export class FoodService {
   // return all food items
   getAll():String[]{
     return [
-      '/assets/images/foods/food1.jpeg'
+      '/assets/images/foods/food1.jpeg',
+      '/assets/images/foods/food2.jpg',
+      '/assets/images/foods/food3.jpeg',
+      '/assets/images/foods/food4.jpg',
+      '/assets/images/foods/food5.jpg',
+      '/assets/images/foods/food6.jpeg'
     ]
   }
 }
