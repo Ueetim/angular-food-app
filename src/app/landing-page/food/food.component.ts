cc8fa409fd4286918a37493fafe0364fa9170b4a
uduak <lorduduak@gmail.com>
2022-09-28 21:10:07 +0100

    food component UI

--- src/app/landing-page/food/food.component.ts
+++ src/app/landing-page/food/food.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FoodService } from 'src/app/services/food/food.service';
+import { Food } from 'src/app/shared/models/food';
+
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodComponent implements OnInit {
 
-  constructor() { }
+  foods:Food[];
+
+  constructor(private foodService:FoodService) {
+    this.foods = foodService.getAll().filter(food => food.stars > 4.0)
+  }
 
   ngOnInit(): void {
   }
