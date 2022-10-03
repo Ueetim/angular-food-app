21c1f2637533af34f30adb1ca26fb0411b5beea5
uduak <lorduduak@gmail.com>
2022-10-03 17:40:59 +0100

    fade animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
